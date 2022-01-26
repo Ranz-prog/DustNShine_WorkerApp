238f822196463051a3b4dd1e8ea1823d33228fba
addealbuera <80665727+addealbuera@users.noreply.github.com>
2022-01-26 16:35:28 +0800

    Layout Update
    Added Custom pop up message on service details

--- app/src/main/java/com/example/dnsworker/ServiceDetails.java
+++ app/src/main/java/com/example/dnsworker/ServiceDetails.java
@@ -1,22 +1,35 @@
 package com.example.dnsworker;
 
 import android.app.Activity;
+import android.app.Dialog;
+import android.app.FragmentManager;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
 
 public class ServiceDetails extends Activity {
 
     private ImageView serviceBackButton;
+    private Button doneWorkBtn;
+
+    private Dialog dialog;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_service_details);
 
         serviceBackButton = findViewById(R.id.service_arrowback);
+        doneWorkBtn = findViewById(R.id.doneWorkButton);
+        dialog = new Dialog(this);
 
         serviceBackButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -24,5 +37,29 @@ public class ServiceDetails extends Activity {
                 finish();
             }
         });
+
+        doneWorkBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openDialog();
+            }
+        });
     }
+    private void openDialog(){
+        dialog.setContentView(R.layout.dialog_confirmation);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+        Button btnDone = (Button) dialog.findViewById(R.id.confirmButton);
+        btnDone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainMenu.class);
+                startActivity(intent);
+                finish();
+                dialog.dismiss();
+            }
+        });
+        dialog.show();
+    }
+
 }
